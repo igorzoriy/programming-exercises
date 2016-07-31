61af9eae31e77123cd38359c0428faa8144c91cb
Igor Zoriy <gh@igorzoriy.com>
2016-07-31 11:57:03 +0200

    Add euler3 on Go

---
+++ euler2.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+    sum := 0
+
+    fibonacci := []int{0, 1}
+
+    for i := 2; ; i++ {
+        current := fibonacci[i - 2] + fibonacci[i - 1]
+
+        if current > 4000000 {
+            break
+        }
+
+
+        if current % 2 == 0 {
+            sum += current
+        }
+        fibonacci = append(fibonacci, current)
+    }
+
+    fmt.Println(sum)
+}
